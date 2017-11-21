bc9329f02a8c7ba888c0b68f52eaeaa02dac3c5e
dev_bpn <jazzbpn@gmail.com>
2017-11-21 13:17:25 +0545

    get api with query

--- app/src/main/java/com/mobotechnology/bipinpandey/retrofit_handdirty/my_interface/GetNoticeDataService.java
+++ app/src/main/java/com/mobotechnology/bipinpandey/retrofit_handdirty/my_interface/GetNoticeDataService.java
@@ -4,10 +4,18 @@ import com.mobotechnology.bipinpandey.retrofit_handdirty.model.NoticeList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface GetNoticeDataService {
 
     @GET("bins/1bsqcn/")
     Call<NoticeList> getNoticeData();
+
+    /**
+     * @since Not used, Just to know how to use @query to get JSONObject
+     * Get data using query
+     * */
+    @GET("bins/path/")
+    Call<NoticeList> getNoticeDataData(@Query("company_no") int companyNo);
 
 }
